25dfebbf9046464be311aac436efa59d01e4c33f
brianc725 <briandy381@aol.com>
2017-08-11 21:18:09 -0700

    Initial Commit

---
+++ Battleship/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Battleship
+//
+//  Created by Brian Chan on 8/11/17.
+//  Copyright © 2017 Brian Chan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
